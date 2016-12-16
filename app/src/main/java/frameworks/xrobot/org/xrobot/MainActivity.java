2040bf31744bc9281420372afd533a2745d0a08d
TengWang <markzog21@gmail.com>
2016-12-16 09:59:51 +0800

    Build Project

---
+++ app/src/main/java/frameworks/xrobot/org/xrobot/MainActivity.java
@@ -0,0 +1,13 @@
+package frameworks.xrobot.org.xrobot;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
